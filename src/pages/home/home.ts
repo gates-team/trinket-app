e8de841f51ee68d8f092770159f2b14051666a84
LAPTOP-VJLHVE5S\brand <brandonluizz@hotmail.com>
2017-08-19 00:49:37 -0300

    add HttpModule e HomeProvider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HomeProvider } from '../../providers/home/home';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [HomeProvider]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private homeProvider: HomeProvider) {
+    this.homeProvider.testTrinketApi();
   }
-
 }
---
+++ src/providers/home/home.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the HomeProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular DI.
+*/
+@Injectable()
+export class HomeProvider {
+  
+  constructor(public http: Http) {
+
+  }
+
+  testTrinketApi() {
+    return this.http.get('http://stg-hubapps.mundipagg.com:5002/')
+    .subscribe(data => data.json())
+  }
+}
