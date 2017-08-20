14800e8331c01408f530452bdbd8b546f66a81c2
Thiago Barradas <thiagobrowskt@gmail.com>
2017-08-20 04:54:04 -0300

    facebook

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -24,13 +24,33 @@ export class LoginPage {
   }
 
   login () {
-    /*this.fb.login(['public_profile', 'user_friends', 'email'])
+
+    this.fb.getLoginStatus().then((FacebookStatusResponse) => {
+      if(FacebookStatusResponse.status == 'connected')
+      {
+        console.log('Already logged into Facebook!', FacebookStatusResponse)
+        this.navCtrl.setRoot('Home',FacebookStatusResponse)
+      }
+      else
+      {
+        this.loginCore();
+      }
+    });
+
+   
+  }
+
+  loginCore() {
+   this.fb.login(['public_profile', 'user_friends', 'email'])
     .then((res: FacebookLoginResponse) => {
       console.log('Logged into Facebook!', res)
       this.navCtrl.setRoot('Home',res)
     })
-    .catch(e => console.log('Error logging into Facebook', e));*/
-    this.navCtrl.setRoot('Home')
+    .catch(e => console.log('Error logging into Facebook', e));
+  }
+
+  ionViewWillEnter () {
+    this.login();
   }
 
 }
